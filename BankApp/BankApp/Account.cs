1bf0946a50d0277c801a6e81025a7664fd5e132f
ozgekb <ozge.kantar@gmail.com>
2020-04-21 23:12:05 -0700

    starting to write bank app

---
+++ BankApp/BankApp/Account.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace BankApp
+{
+    /// <summary>
+    /// Summery of this class
+    /// this class represent a bank account
+    /// where you can deposit and withdraw
+    /// </summary>
+    class Account
+    {
+        #region Properties
+        /// <summary>
+        /// this property
+        /// Uniq account number for the account 
+        /// </summary>
+        public int AccountNumber { get; set; }
+        /// <summary>
+        /// Email adress of the account holder
+        /// </summary>
+        public string EmailAdress { get; set; }
+        public string AccountName { get; set; }
+        public string AccountType { get; set; }
+        public decimal Balance { get; set; }
+        public DateTime CreatedDate { get; set; }
+        #endregion
+        #region Methods
+        /// <summary>
+        /// Deposit money into your account
+        /// </summary>
+        /// <param name="amount">amount to deposit </param>
+        public void Deposit(decimal amount)
+        {
+            // Balance = Balance + amount;
+               Balance += amount;
+        }
+        public decimal Withdraw(decimal amount)
+        {
+            Balance -= amount;
+            return Balance;
+        }
+        #endregion
+    }
+}
--- BankApp/BankApp/Program.cs
+++ BankApp/BankApp/Program.cs
@@ -6,7 +6,18 @@ namespace BankApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // first create an account so that we will going to create a instance of account class
+            var myFirstAccount = new Account();
+            myFirstAccount.AccountNumber = 1234;
+            myFirstAccount.AccountName = "My checking";
+            myFirstAccount.AccountType = "Checking";
+            myFirstAccount.EmailAdress = "test@test.com";
+            myFirstAccount.Deposit(123456);
+
+            var mySecondAcount = new Account();
+            mySecondAcount.AccountNumber = 45678;
+            mySecondAcount.AccountName = "My savings";
+
         }
     }
 }
