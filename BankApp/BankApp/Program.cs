6e3d52db959f6aa4e75b403d79fdfb1aabad2473
ozgekb <ozge.kantar@gmail.com>
2020-04-22 22:27:02 -0700

    made some changes

--- BankApp/BankApp/Program.cs
+++ BankApp/BankApp/Program.cs
@@ -8,7 +8,7 @@ namespace BankApp
         {
             // first create an account so that we will going to create a instance of account class
             var myFirstAccount = new Account();
-            myFirstAccount.AccountNumber = 1234;
+            //myFirstAccount.AccountNumber = 1234; will be generated automaticly random number not a good idea because we should check that same number wouldnt used
             myFirstAccount.AccountName = "My checking";
             myFirstAccount.AccountType = "Checking";
             myFirstAccount.EmailAdress = "test@test.com";
@@ -19,7 +19,7 @@ namespace BankApp
             Console.WriteLine($"AN:{myFirstAccount.AccountNumber}, Name :{ myFirstAccount.AccountName}, Email:{myFirstAccount.EmailAdress}, AccountType : {myFirstAccount.AccountType}, Balance: {myFirstAccount.Balance}, CD:{myFirstAccount.CreatedDate}");
            
             var mySecondAccount = new Account();
-            mySecondAccount.AccountNumber = 45678;
+            //mySecondAccount.AccountNumber = 45678;
             mySecondAccount.AccountName = "My savings";
             Console.WriteLine($"AN:{mySecondAccount.AccountNumber}, Name :{mySecondAccount.AccountName}, Email:{mySecondAccount.EmailAdress}, AccountType : {mySecondAccount.AccountType}, Balance: {mySecondAccount.Balance}, CD:{mySecondAccount.CreatedDate}");
 
